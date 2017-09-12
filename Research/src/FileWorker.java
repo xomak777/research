293de5d113d7355374059274b0db3ffe9ecbb1ed
innopolis <innopolis@innopolis-desktop>
2017-09-13 02:43:31 +0300

    new

---
+++ Research/src/FileWorker.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class FileWorker {
+ 
+	public static void listFilesForFolder(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	            listFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	        }
+	    }
+	}
+
+	
+	 public static void main(String[] args) {
+			final File folder = new File("/home/innopolis/Desktop");
+			listFilesForFolder(folder);
+	    }
+
+
+	
+}
